894722deeb5cee7aab49c719e149b454213ec2f0
Your Name <you@example.com>
2026-04-14 20:40:05 +0100

    init: mise en place du projet TP formulaires PHP

---
+++ condidature.php
@@ -0,0 +1,19 @@
+<?php
+$prenom     = '';
+$nom        = '';
+$email      = '';
+$age        = '';
+$filiere    = '';
+$motivation = '';
+$erreurs    = [];
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Candidature</title>
+</head>
+<body>
+
+</body>
+</html>
